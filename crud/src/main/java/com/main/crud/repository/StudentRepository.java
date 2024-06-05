9fa20316697073183ef4765349e77d1b09f5e799
duranangie <duranangie>
2024-06-04 23:52:32 -0500

    feat: :sparkles: repository

--- crud/src/main/java/com/main/crud/repository/StudentRepository.java
+++ crud/src/main/java/com/main/crud/repository/StudentRepository.java
@@ -5,7 +5,11 @@ import com.main.crud.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student ,Long> {
+
+    Optional<Student> findById(Long id);
 
 }
